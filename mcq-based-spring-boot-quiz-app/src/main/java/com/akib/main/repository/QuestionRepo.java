3c88ebf5a8780ae7f195716b2ad033c557727ba5
Shoeb Akibul Islam <59358725+Bill-Board@users.noreply.github.com>
2023-09-10 18:36:39 +0600

    Add files via upload

---
+++ mcq-based-spring-boot-quiz-app/src/main/java/com/akib/main/repository/QuestionRepo.java
@@ -0,0 +1,11 @@
+package com.akib.main.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.akib.main.model.Question;
+
+@Repository
+public interface QuestionRepo extends JpaRepository<Question, Integer> {
+	
+}
