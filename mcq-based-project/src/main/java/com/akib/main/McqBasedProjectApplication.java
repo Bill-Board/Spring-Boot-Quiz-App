9647f3efdc424dff999a93a77a942e93873e27b2
Shoeb Akibul Islam <59358725+Bill-Board@users.noreply.github.com>
2023-09-14 15:50:09 +0600

    Add files via upload

---
+++ mcq-based-project/src/main/java/com/akib/main/McqBasedProjectApplication.java
@@ -0,0 +1,13 @@
+package com.akib.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McqBasedProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McqBasedProjectApplication.class, args);
+	}
+
+}
